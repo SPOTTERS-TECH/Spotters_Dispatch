11548a477d457f9910863bdf2440ebe155ee0831
samuel <samfrosh673@gmail.com>
2023-01-02 15:45:03 +0100

    updated notification to live

--- app/src/main/java/com/example/spottersdispatch/Notification.java
+++ app/src/main/java/com/example/spottersdispatch/Notification.java
@@ -1,21 +1,39 @@
 package com.example.spottersdispatch;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Notification extends AppCompatActivity implements Notification_RecyclerAdapter.ItemClickListener {
     RecyclerView recyclerView;
     Notification_RecyclerAdapter adapter;
     String fid;
     private ArrayList<complaint_Product> userList = new ArrayList<>();
+    final static String load_items = "https://spotters.tech/dispatch-it/android/rider_notification.php";
 
 
     SharedPreferences sharedPreferences;
@@ -41,75 +59,63 @@ public class Notification extends AppCompatActivity implements Notification_Recy
         recyclerView.setLayoutManager(layoutManager);
         layoutManager.setOrientation(RecyclerView.VERTICAL);
 
-        adapter = new Notification_RecyclerAdapter(this,userList);
+        adapter = new Notification_RecyclerAdapter(this, userList);
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
 
-        build();
+        load_items();
 
     }
 
-    private void build() {
 
-        userList.add(new complaint_Product("information to all staff","2022-04-06","we will be having a app maintainance from around 12am to 8am on thursday july 2022,you will not be able to access the application during the maintainance period. Thank you!"));
-        userList.add(new complaint_Product("application upgrade ","2022-04-01","we will be having a app maintainance from around 10pm to 8am on thursday july 2022,you will not be able to access the application during the maintainance period. Thank you!"));
-        userList.add(new complaint_Product("information to all staff","2022-04-06","we will be having a app maintainance from around 12am to 8am on thursday july 2022,you will not be able to access the application during the maintainance period. Thank you!"));
-        userList.add(new complaint_Product("application upgrade ","2022-04-01","we will be having a app maintainance from around 10am to 8am on thursday july 2022,you will not be able to access the application during the maintainance period. Thank you!"));
+    private void load_items() {
+        String id = fid;
 
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, load_items, new Response.Listener<String>() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject object = jsonArray.getJSONObject(i);
+                        String title = object.getString("title").trim();
+                        String date = object.getString("date").trim();
+                        String subtext = object.getString("message").trim();
+
+                        complaint_Product product = new complaint_Product(title, date, subtext);
+
+                        userList.add(0, product);
+                    }
+
+
+                    adapter = new Notification_RecyclerAdapter(Notification.this, userList);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                    int count = 0;
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "error" + e + toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+//                Toast.makeText(getContext(), "error"+ error+toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<>();
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
     }
-
-//    private void load_items() {
-//        String id = fid;
-//
-//
-//        StringRequest stringRequest = new StringRequest(Request.Method.POST, load_items, new Response.Listener<String>() {
-//            @Override
-//            public void onResponse(String response) {
-//                try {
-//                    JSONObject jsonObject = new JSONObject(response);
-//                    String success = jsonObject.getString("success");
-//                    JSONArray jsonArray = jsonObject.getJSONArray("read");
-//                    if (success.equals("1")) {
-//                        for (int i = 0; i < jsonArray.length(); i++) {
-//                            JSONObject object = jsonArray.getJSONObject(i);
-//                            String title = object.getString("order_id").trim();
-//                            String date = object.getString("rec_name").trim();
-//                            String subtext = object.getString("rec_phone").trim();
-//
-//
-//                            complaint_Product product = new complaint_Product(title,date,subtext);
-//                            userList.clear();
-//                            userList.add(product);
-//
-//                        }
-//
-//                    }
-//                    adapter = new complaint_RecyclerAdapter(Notification.this,userList);
-//                    recyclerView.setAdapter(adapter);
-//
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                    Toast.makeText(Notification.this, "error"+ e+toString(), Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                Toast.makeText(Notification.this, "Error" +error.toString(), Toast.LENGTH_SHORT).show();
-//            }
-//        }){
-//            @Nullable
-//            @Override
-//            protected Map<String, String> getParams() throws AuthFailureError {
-//                HashMap<String, String> params = new HashMap<>();
-//                params.put("id", id);
-//                return params;
-//            }
-//        };
-//
-//        RequestQueue requestQueue = Volley.newRequestQueue(this);
-//        requestQueue.add(stringRequest);
-//    }
 
     public void backbtn(View view) {
         onBackPressed();
